0e2941eac0d9b7c0e5d9dc8221bb75b9713550b6
Sunil Mekala <sunil.rao15@gmail.com>
2015-12-14 12:51:51 +0530

    completed services page using bootstrap

---
+++ assignment/js/app-script.js
@@ -0,0 +1,16 @@
+/*
+
+My Custom JS
+============
+
+Author:  Sunil Mekala
+Updated: December 2015
+Notes:	 Hand coded for learning
+
+*/
+
+$(function(){
+
+// Bootstrap tooltip function
+	$("[rel='tooltip']").tooltip();
+});
